641c4d33ef727abfab63096082141adee5a0c45c
German Gamboa <thegerman@Germans-MacBook-Pro.local>
2018-03-03 00:47:06 -0500

    taco shop page

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('I work!');
+let current;
+let past;
+let items = document.getElementById('menu');
+
+items.addEventListener('click', e => {
+  past = current;
+  current = e.target.parentElement;
+  current.classList.add('visible');
+  if(past != undefined){
+    past.classList.remove('visible');
+  }
+});
